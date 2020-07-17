05cb168f315123e0cabfc0733a527d68903b9fce
Michal Kukla <michal.kukla@tul.cz>
2020-07-17 15:04:25 +0200

    Added patch fix borders for transparency, other binding keys added

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 10;       /* gap pixel between windows */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -59,11 +59,21 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *muttcmd[] = { "st", "-e", "neomutt", NULL };
+static const char *browsercmd[] = { "qutebrowser", "--temp-basedir", NULL };
+
+static const char *poweroffcmd[] = { "i3exit", "shutdown", NULL};
+static const char *suspendcmd[] = { "i3exit", "suspend", NULL };
+static const char *hibernatecmd[] = { "i3exit", "hibernate", NULL };
+static const char *rebootcmd[] = { "i3exit", "reboot", NULL };
+static const char *turn_monitor_off_cmd[] = { "i3exit", "turn_monitor_off", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,		XK_m, 	   spawn,	   {.v = muttcmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY, 			XK_w,      spawn,	   {.v = browsercmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
