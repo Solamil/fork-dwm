73c964954789703df4e8ef8dd021650ddf85a872
Michal Kukla <michal.kukla@tul.cz>
2020-07-22 08:03:31 +0200

    poweroff, suspend, hibernate, turn monitor off added

--- config.h
+++ config.h
@@ -45,7 +45,8 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod4Mask 
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -60,7 +61,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *muttcmd[] = { "st", "-e", "neomutt", NULL };
-static const char *browsercmd[] = { "qutebrowser", "--temp-basedir", NULL };
+static const char *browsercmd[] = { "qutebrowser", NULL };
 
 static const char *poweroffcmd[] = { "i3exit", "shutdown", NULL};
 static const char *suspendcmd[] = { "i3exit", "suspend", NULL };
@@ -71,9 +72,14 @@ static const char *turn_monitor_off_cmd[] = { "i3exit", "turn_monitor_off", NULL
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,		XK_m, 	   spawn,	   {.v = muttcmd} },
+	{ MODKEY,			XK_m, 	   spawn,	   {.v = muttcmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY, 			XK_w,      spawn,	   {.v = browsercmd} },
+	{ MODKEY, 			XK_F1, 	   spawn,	   {.v = suspendcmd} },
+	{ MODKEY, 			XK_F2, 	   spawn,	   {.v = hibernatecmd} },
+	{ MODKEY, 			XK_F3, 	   spawn,	   {.v = rebootcmd} },
+	{ MODKEY, 			XK_F4, 	   spawn,	   {.v = poweroffcmd} },
+	{ MODKEY, 			XK_F5, 	   spawn,	   {.v = turn_monitor_off_cmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -86,7 +92,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
